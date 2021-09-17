d245c4f2870a71810b7e215cfe578a0bd158e6db
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2021-09-17 15:05:12 -0700

    Adding images to the test script

--- test.sh
+++ test.sh
@@ -1,9 +1,49 @@
 #! /bin/bash
 
 IMAGES=(
-    'openjdk:8-jdk-alpine'
     'adoptopenjdk:8-jre-hotspot'
     'adoptopenjdk:8-jre-openj9'
+    'adoptopenjdk:11-jre-hotspot'
+    'adoptopenjdk:11-jre-openj9'
+    'adoptopenjdk:16-jre-hotspot' # TODO: 17
+    'adoptopenjdk:16-jre-openj9'  # TODO: 17
+
+    'eclipse-temurin:8-jre'
+    'eclipse-temurin:11-jre'
+    'eclipse-temurin:16-jdk' # TODO 17 (no 16-jre)
+
+    'amazoncorretto:8'
+    'amazoncorretto:8-alpine'
+    'amazoncorretto:11'
+    'amazoncorretto:11-alpine'
+    'amazoncorretto:17'
+    'amazoncorretto:17-alpine'
+
+    'bellsoft/liberica-openjre-debian:8'
+    'bellsoft/liberica-openjre-alpine-musl:8'
+    'bellsoft/liberica-openjre-debian:11'
+    'bellsoft/liberica-openjre-alpine-musl:11'
+    'bellsoft/liberica-openjre-debian:16'      # TODO: 17
+    'bellsoft/liberica-openjre-alpine-musl:16' # TODO: 17
+
+    'openjdk:8-jre-slim'
+    # 8 alpine was discontinued
+    'openjdk:11-jre-slim'
+    # 11 alpine is not available
+    'openjdk:17-slim'
+    'openjdk:17-alpine'
+    'openjdk:18-slim'
+    'openjdk:18-alpine'
+
+    'sapmachine:11'
+    'sapmachine:17'
+
+    'azul/zulu-openjdk:8'
+    'azul/zulu-openjdk:11'
+    'azul/zulu-openjdk:16' # TODO: 17
+    'azul/zulu-openjdk-alpine:8-jre'
+    'azul/zulu-openjdk-alpine:11-jre'
+    'azul/zulu-openjdk-alpine:16-jre' # TODO: 17
 )
 
 OUTPUT_DIR='test-out'
